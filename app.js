bfe53ff6766e9fd4683cd45df3ac97b45ee23396
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-11-05 17:47:05 -0800

    delpoy sprint04

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const cors = require('cors')  // using this module to solve CORS problem
 // note the extra line in package.json to download this code
 
 var corsOptions = {
-origin: 'http://localhost:4200',
-//origin: 'https://isit422clientfall2019.azurewebsites.net',   // this URL must match the URL that the Angular app will call from
+//origin: 'http://localhost:4200',
+origin: 'https://bigotbotclientfall2019.azurewebsites.net',   // this URL must match the URL that the Angular app will call from
 //origin: 'kurtangularappfall2019.azurewebsites.net',   // this URL must match the URL that the Angular app will call from
 optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
